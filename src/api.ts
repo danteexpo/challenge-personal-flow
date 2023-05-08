1224779897107e2319787cb7260bd493ccd25b14
Dante Expósito <dante0im@gmail.com>
2023-05-08 20:45:09 -0300

    Fixed API

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
 export const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?';
+
 export const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'c9461d7c9dmsh46aa0bce84e796bp1590c7jsn6d0eb1ae0323',
+    'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
     'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
   },
 };
